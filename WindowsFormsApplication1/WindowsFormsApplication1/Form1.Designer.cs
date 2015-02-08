bfac282150f58e1439f49e6bb44bd17766fb701d
meishine <meishine1370@gmail.com>
2015-02-08 15:12:13 +0800

    不明解決後的推送

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -64,11 +65,22 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "惡搞!!!";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(409, 120);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "惡搞!!!";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(756, 462);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -85,6 +97,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
